c8fe91416ba9076a73582a0bffe5a83a7f626ca6
Alex Regan <alex.joseph.regan@gmail.com>
2018-06-09 08:16:25 -0700

    feat: chaining returns new instances

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,7 +2,13 @@ import { Currency } from "./index";
 
 describe("Currency", async () => {
   it("should work", async () => {
-    let c = new Currency(9.99);
+    let c = Currency.fromPrice(9.99);
     expect(c.add(1.01).toString()).toBe("11");
+    expect(c.add(1.01).toNumber()).toBe(11);
+    c = Currency.fromString("$1023.99");
+    expect(c.toString()).toBe("1,023.99");
+    expect(c.setLocaleOptions({ useGrouping: false }).toString()).toBe(
+      "1023.99"
+    );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,20 @@ function toDollars(pennies: number): number {
   return pennies / CurrencyCoefficient;
 }
 
+function parseCurrency(price: string | number): number {
+  let c = typeof price == "string" ? parseFloat(price) : price;
+  if (Number.isNaN(c)) {
+    return 0.0;
+  }
+
+  return c;
+}
+
+const regex = /[^\d.]/g;
+function stripToNumeric(x: string): string {
+  return x.replace(regex, "");
+}
+
 export interface CurrencyOptions {
   locales: string | string[] | undefined;
   localeOptions: Intl.NumberFormatOptions | undefined;
@@ -20,42 +34,87 @@ export interface CurrencyOptions {
  * Currency.
  */
 export class Currency {
+  static stringParser = stripToNumeric;
   /**
    * Internal value in pennies.
    */
   private value: number;
-  private numFmt: Intl.NumberFormat;
+  private locales: string | string[] | undefined;
+  private localeOptions: Intl.NumberFormatOptions | undefined;
 
-  constructor(price: number, options: Partial<CurrencyOptions> = {}) {
+  constructor(pennies: number, options: Partial<CurrencyOptions> = {}) {
     let { locales = "en-US", localeOptions = {} } = options;
-    this.value = toPennies(price);
-    this.numFmt = new Intl.NumberFormat(locales, {
-      currency: "USD",
-      ...localeOptions,
-    });
+    this.value = pennies;
+    this.locales = locales;
+    this.localeOptions = localeOptions;
   }
 
-  public add: (price: number) => this = price => {
-    this.value += toPennies(price);
+  public setLocales(...locales: string[]): this {
+    this.locales = locales;
     return this;
+  }
+
+  public setLocaleOptions(options: Intl.NumberFormatOptions): this {
+    this.localeOptions = options;
+    return this;
+  }
+
+  public add: (price: number) => Currency = price => {
+    return new Currency(this.value + toPennies(price), {
+      locales: this.locales,
+      localeOptions: this.localeOptions
+    });
   };
 
-  public subtract: (price: number) => this = price => {
-    this.value -= toPennies(price);
-    return this;
+  public subtract: (price: number) => Currency = price => {
+    return new Currency(this.value - toPennies(price), {
+      locales: this.locales,
+      localeOptions: this.localeOptions
+    });
   };
 
-  public sub = this.subtract;
-
-  public divide: (divisor: number) => this = amount => {
+  public divide: (divisor: number) => Currency = amount => {
     // Value as pennies doesn't require transformation for a divisor.
-    this.value /= amount;
-    return this;
+    return new Currency(this.value / amount, {
+      locales: this.locales,
+      localeOptions: this.localeOptions
+    });
+  };
+
+  public multiply: (factor: number) => Currency = factor => {
+    // Value as pennies doesn't require transformation for a factor.
+    return new Currency(this.value * factor, {
+      locales: this.locales,
+      localeOptions: this.localeOptions
+    });
   };
 
   public toString: () => string = () => {
-    return this.numFmt.format(this.toNumber());
+    return new Intl.NumberFormat(this.locales, this.localeOptions).format(
+      this.toNumber()
+    );
   };
 
   public toNumber: () => number = () => toDollars(this.value);
+
+  public toJSON(): number {
+    return this.toNumber();
+  }
+
+  public static fromPrice(
+    price: number,
+    options: Partial<CurrencyOptions> = {}
+  ): Currency {
+    return new Currency(toPennies(price), options);
+  }
+
+  public static fromString(
+    price: string,
+    options: Partial<CurrencyOptions> = {}
+  ): Currency {
+    return new Currency(
+      toPennies(parseCurrency(Currency.stringParser(price))),
+      options
+    );
+  }
 }
